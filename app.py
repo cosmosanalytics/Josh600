7bd15cd6581f8901a104c9c56ccb52cf58766240
cosmosanalytics <100080151+cosmosanalytics@users.noreply.github.com>
2023-01-09 10:12:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 import streamlit as st
 import pandas as pd
+st.set_page_config(layout="wide")
+st.title('Josh 600 Actual vs Model')
 
 @st.cache
 def loadData():
     s = pd.read_csv('Josh_s.csv', index_col = 0)
     p = pd.read_csv('Josh_s.csv', index_col = 0)
-    sp = pd.read_csv('Josh_s_perf.csv', index_col = 0)
-    pp = pd.read_csv('Josh_p_perf.csv', index_col = 0)
-    return s, p, sp, pp
-  
-s, p, sp, pp = loadData()
-st.write(s)
+    return s, p
+ 
+s, p = loadData()
+col1, col2 = st.columns(2)
+with col1:
+    fig, ax = plt.subplots(figsize=(20,10))
+    st.title('Production')
+    gp = st.multiselect('Select plant-generic combination', p.columns)
+    p[gp].plot()
+    st.pyplot(fig)
+with col2:
+    fig, ax = plt.subplots(figsize=(20,10))
+    st.title('Shipment')
+    gp = st.multiselect('Select plant-generic combination', p.columns)
+    s[gp].plot()
+    st.pyplot(fig)
+     
