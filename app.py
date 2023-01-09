6604845a496b8dd40962d961f3a2457a90dbf671
cosmosanalytics <100080151+cosmosanalytics@users.noreply.github.com>
2023-01-09 09:50:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import streamlit as st
 import pandas as pd
 
-st.write('hello')
+@st.cache
+def loadData():
+    s = pd.read_csv('Josh_s.csv', index_col = 0)
+    p = pd.read_csv('Josh_s.csv', index_col = 0)
+    sp = pd.read_csv('Josh_s_perf.csv', index_col = 0)
+    pp = pd.read_csv('Josh_p_perf.csv', index_col = 0)
+    return s, p, sp, pp
+  
+s, p, sp, pp = loadData()
+st.write(s)
