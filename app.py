cc7373f19b6b6ca2e850681cffbf2c0a9d2d4b0a
cosmosanalytics <100080151+cosmosanalytics@users.noreply.github.com>
2023-01-09 10:35:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,25 +6,25 @@ st.title('Josh 600 Actual vs Model')
 
 @st.cache
 def loadData():
-    s = pd.read_csv('Josh_s.csv', index_col = 0); s.index = pd.to_datetime(s.index)
-    p = pd.read_csv('Josh_s.csv', index_col = 0); p.index = pd.to_datetime(p.index)
+    s = pd.read_csv('Josh_s.csv', index_col = 0);
+    p = pd.read_csv('Josh_p.csv', index_col = 0); 
     return s, p
  
 s, p = loadData()
 col1, col2 = st.columns(2)
 with col1:
-    pl = st.selectbox('Select production plant-generic combination', pd.Series(p.columns).apply(lambda x: x.split('_')[0]).unique())
+    p1 = st.selectbox('Select production plant-generic combination', pd.Series(p.columns).apply(lambda x: x.split('_')[0]).unique())
     fig, ax = plt.subplots(figsize=(20,10))  
     try:
-        p[p.columns[p.columns.str.contains(pl)]].plot(ax=ax)
+        p[p.columns[p.columns.str.contains(p1)]].plot(ax=ax)
     except:
         pass
     st.pyplot(fig)
 with col2:
-    sl = st.selectbox('Select shipment plant-generic combination', pd.Series(s.columns).apply(lambda x: x.split('_')[0]).unique())
+    s1 = st.selectbox('Select shipment plant-generic combination', pd.Series(s.columns).apply(lambda x: x.split('_')[0]).unique())
     fig, ax = plt.subplots(figsize=(20,10))  
     try:
-        s[s.columns[s.columns.str.contains(sl)]].plot(ax=ax)
+        s[s.columns[s.columns.str.contains(s1)]].plot(ax=ax)
     except:
         pass
     st.pyplot(fig)
