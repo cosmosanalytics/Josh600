8547e429ac347994f19d6153a61c0f41a0e1f24d
cosmosanalytics <100080151+cosmosanalytics@users.noreply.github.com>
2023-01-09 10:46:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,15 +8,18 @@ st.title('Josh 600 Actual vs Model')
 def loadData():
     s = pd.read_csv('Josh_s.csv', index_col = 0);
     p = pd.read_csv('Josh_p.csv', index_col = 0); 
-    return s, p
+    sp = pd.read_csv('Josh_s_perf.csv', index_col = 0);
+    pp = pd.read_csv('Josh_p_perf.csv', index_col = 0);     
+    return s, p, sp,pp
  
-s, p = loadData()
+s, p, sp, pp = loadData()
 col1, col2 = st.columns(2)
 with col1:
     p1 = st.selectbox('Select production plant-generic combination', pd.Series(p.columns).apply(lambda x: x.split('_')[0]).unique())
     fig, ax = plt.subplots(figsize=(20,10))  
     try:
         p[p.columns[p.columns.str.contains(p1)]].plot(ax=ax)
+        ax.set_title(pp[pp.index=p1])
     except:
         pass
     st.pyplot(fig)
