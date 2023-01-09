0ac6380e4188ae385a5656112b17ca3e3f414f94
cosmosanalytics <100080151+cosmosanalytics@users.noreply.github.com>
2023-01-09 11:04:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ with col1:
     p1 = st.selectbox('Select production plant-generic combination', pd.Series(p.columns).apply(lambda x: x.split('_')[0]).unique())
     fig, ax = plt.subplots(figsize=(20,10))    
     try:
-        smape, corrcoef = pp['corr. coef'].loc[p1].str.split('_')[0], pp['corr. coef'].loc[p1].str.split('_')[1]
+        smape, corrcoef = pp.loc[p1,'corr. coef'].str.split('_')[0], pp.loc[p1,'corr. coef'].str.split('_')[1]
         title = 'corr. coef. = '+corrcoef+', smape = '+smape
         st.write(title)
         p[p.columns[p.columns.str.contains(p1)]].plot(ax=ax, title=title)
