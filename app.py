5baef6be31edc1668b3d29d9bbd49a81048937ef
cosmosanalytics <100080151+cosmosanalytics@users.noreply.github.com>
2023-01-10 10:20:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ def loadData():
     pp = pd.read_csv('Josh_p_perf.csv', index_col = 0);     
     return s, p, sp,pp
 
-def ps_plot(ps, ax):
+def ps_plot(ps):
+    fig, ax = plt.subplots(figsize=(10,5))
     ps['col0 avg'] = ps[ps.columns[0]].mean(); ps['col0 std'] = ps[ps.columns[0]].std()
     ps['col0 avg+std'] = ps['col0 avg'] + ps['col0 avg']; ps['col0 avg-std'] = ps['col0 avg'] - ps['col0 std']
     ps['col1 avg'] = ps[ps.columns[1]].mean(); ps['col1 std'] = ps[ps.columns[1]].std()
@@ -22,22 +23,18 @@ def ps_plot(ps, ax):
     ps[['col0 avg','col0 avg+std','col0 avg-std']].plot(ax=ax, color='blue', linestyle = '--', alpha=0.5, legend=None)
     ps[ps.columns[1]].plot(ax=ax, color='orange', legend=True); 
     ps[['col0 avg','col1 avg+std','col1 avg-std']].plot(ax=ax, color='orange', linestyle = '--', alpha=0.5, legend=None)
-    ax.set_title('corr. coef. = '+corrcoef+', smape = '+smape)    
+    ax.set_title('corr. coef. = '+corrcoef+', smape = '+smape) 
+    st.pyplot(fig)
  
 s, p, sp, pp = loadData()
 col1, col2 = st.columns(2)
 with col1:
-    p1 = st.selectbox('Select production plant-generic combination', pd.Series(p.columns).apply(lambda x: x.split('_')[0]).unique())
-    fig, ax = plt.subplots(figsize=(10,5))  
+    p1 = st.selectbox('Select production plant-generic combination', pd.Series(p.columns).apply(lambda x: x.split('_')[0]).unique())  
     try:
         smape, corrcoef = pp.loc[p1,'corr. coef'].split('_')[0], pp.loc[p1,'corr. coef'].split('_')[1]
         ps_plot(p[p.columns[p.columns.str.contains(p1)]])
-        
-
-
     except:
-        pass
-    st.pyplot(fig)
+        pass   
 with col2:
     s1 = st.selectbox('Select shipment plant-generic combination', pd.Series(s.columns).apply(lambda x: x.split('_')[0]).unique())
     fig, ax = plt.subplots(figsize=(10,5))  
