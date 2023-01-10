1808e8d52a196146b8ecd9c39ac118f7b4271a63
cosmosanalytics <100080151+cosmosanalytics@users.noreply.github.com>
2023-01-10 10:19:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,17 @@ def loadData():
     pp = pd.read_csv('Josh_p_perf.csv', index_col = 0);     
     return s, p, sp,pp
 
-# def 
+def ps_plot(ps, ax):
+    ps['col0 avg'] = ps[ps.columns[0]].mean(); ps['col0 std'] = ps[ps.columns[0]].std()
+    ps['col0 avg+std'] = ps['col0 avg'] + ps['col0 avg']; ps['col0 avg-std'] = ps['col0 avg'] - ps['col0 std']
+    ps['col1 avg'] = ps[ps.columns[1]].mean(); ps['col1 std'] = ps[ps.columns[1]].std()
+    ps['col1 avg+std'] = ps['col1 avg'] + ps['col1 avg']; ps['col1 avg-std'] = ps['col1 avg'] - ps['col1 std']
+
+    ps[ps.columns[0]].plot(ax=ax, color='blue', legend=True); 
+    ps[['col0 avg','col0 avg+std','col0 avg-std']].plot(ax=ax, color='blue', linestyle = '--', alpha=0.5, legend=None)
+    ps[ps.columns[1]].plot(ax=ax, color='orange', legend=True); 
+    ps[['col0 avg','col1 avg+std','col1 avg-std']].plot(ax=ax, color='orange', linestyle = '--', alpha=0.5, legend=None)
+    ax.set_title('corr. coef. = '+corrcoef+', smape = '+smape)    
  
 s, p, sp, pp = loadData()
 col1, col2 = st.columns(2)
@@ -21,18 +31,9 @@ with col1:
     fig, ax = plt.subplots(figsize=(10,5))  
     try:
         smape, corrcoef = pp.loc[p1,'corr. coef'].split('_')[0], pp.loc[p1,'corr. coef'].split('_')[1]
-        p_plot = p[p.columns[p.columns.str.contains(p1)]]
+        ps_plot(p[p.columns[p.columns.str.contains(p1)]])
         
-        p_plot['col0 avg'] = p_plot[p_plot.columns[0]].mean(); p_plot['col0 std'] = p_plot[p_plot.columns[0]].std()
-        p_plot['col0 avg+std'] = p_plot['col0 avg'] + p_plot['col0 avg']; p_plot['col0 avg-std'] = p_plot['col0 avg'] - p_plot['col0 std']
-        p_plot['col1 avg'] = p_plot[p_plot.columns[1]].mean(); p_plot['col1 std'] = p_plot[p_plot.columns[1]].std()
-        p_plot['col1 avg+std'] = p_plot['col1 avg'] + p_plot['col1 avg']; p_plot['col1 avg-std'] = p_plot['col1 avg'] - p_plot['col1 std']
-        
-        p_plot[p_plot.columns[0]].plot(ax=ax, color='blue', legend=True); 
-        p_plot[['col0 avg','col0 avg+std','col0 avg-std']].plot(ax=ax, color='blue', linestyle = '--', alpha=0.5, legend=None)
-        p_plot[p_plot.columns[1]].plot(ax=ax, color='orange', legend=True); 
-        p_plot[['col0 avg','col1 avg+std','col1 avg-std']].plot(ax=ax, color='orange', linestyle = '--', alpha=0.5, legend=None)
-        ax.set_title('hihiicorr. coef. = '+corrcoef+', smape = '+smape)
+
 
     except:
         pass
